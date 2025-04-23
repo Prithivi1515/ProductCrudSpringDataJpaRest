39fa67d7a2caf909c328e5c158dad16a1fc40199
Prithiviraj <Prithiviraj1515@gmail.com>
2025-04-23 10:03:40 +0530

    day 1 task completed

--- ProductCrudSpringDataJpaRest/src/main/java/com/cts/ProductCrudSpringDataJpaRestApplication.java
+++ ProductCrudSpringDataJpaRest/src/main/java/com/cts/ProductCrudSpringDataJpaRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductCrudSpringDataJpaRestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Day 1 changes");
 		SpringApplication.run(ProductCrudSpringDataJpaRestApplication.class, args);
 	}
 
